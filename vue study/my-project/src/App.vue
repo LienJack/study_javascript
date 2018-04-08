10735fd19e65849ebeeaa0bad519cc2e7bd5735b
blacksnow233 <longyuhuhao@126.com>
2018-04-08 23:06:10 +0800

    准备做保存

--- vue study/my-project/src/App.vue
+++ vue study/my-project/src/App.vue
@@ -1,22 +1,49 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+   <h1 v-text="title"></h1>
+   <input v-model="newItem" v-on:keyup.enter="addNew">
+   <ul>
+     <li v-for="item in items" 
+     :class="{finished:item.isFinished}"
+     v-on:click="toggleFinsh(item)">
+       {{item.label}}
+      </li>
+   </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data:function(){
+    return{
+      title:'this is a todo list',
+      items: [
+      ],
+      newItem:''
+    }
+  },
+  methods:{
+    toggleFinsh:function(item){
+      item.isFinished=!item.isFinished;
+    },
+    addNew:function(){
+      this.items.push({
+        label:this.newItem,
+        isFinished:false
+      })
+      this.newItem=''
+    }
   }
+  
 }
 </script>
 
 <style>
+ul,li{list-style: none;}
+.finished{
+  text-decoration:line-through;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
