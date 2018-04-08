9fd96ca1721bc0193a7f6607d2816444f66e7838
blacksnow233 <longyuhuhao@126.com>
2018-04-08 11:19:34 +0800

    开始vue学习
    开始vue学习

--- DOM study/alert模拟/js/alert.js
+++ DOM study/alert模拟/js/alert.js
@@ -1,25 +1,25 @@
-var myConfirm=function(context,callback,config){
+var myConfirm = function( context, callback, config){
 
   config = {
   	cancelText: config && config.cancelText || "取消",
   	confirmText: config && config.confirmText || "确定",
   }
 
-  var $simpleAlert = $('<div class="simpleAlert">');//wrap容器
-  var $shelter = $('<div class="simpleAlertShelter">');//这是遮罩
-  var $simpleAlertBody = $('<div class="simpleAlertBody">');//弹窗容器
+  var $simpleAlert = $('<div class="simpleAlert">'); // wrap容器
+  var $shelter = $('<div class="simpleAlertShelter">'); // 这是遮罩
+  var $simpleAlertBody = $('<div class="simpleAlertBody">'); // 弹窗容器
   var $simpleAlertBodyClose = $('<img class="simpleAlertBodyClose" src="img/close.png" height="14" width="14"/>');
-  var $simpleAlertBodyContent = $('<p class="simpleAlertBodyContent">' +context+ '</p>');//内容
-  var $btn1 = $('<button class="simpleAlertBtn simpleAlertBtn1">'+config.confirmText+'</button>');//按钮1
-  var $btn2 = $('<button class="simpleAlertBtn simpleAlertBtn2">'+config.cancelText+'</button>');//按钮2
+  var $simpleAlertBodyContent = $('<p class="simpleAlertBodyContent">' + context + '</p>'); // 内容
+  var $btn1 = $('<button class="simpleAlertBtn simpleAlertBtn1">'+ config.confirmText + '</button>'); // 按钮1
+  var $btn2 = $('<button class="simpleAlertBtn simpleAlertBtn2">' + config.cancelText +'</button>'); // 按钮2
   $simpleAlertBody.append($simpleAlertBodyClose)
 				.append($simpleAlertBodyContent)
 				.append($btn1)
 				.append($btn2);
   $simpleAlert.append($shelter).append($simpleAlertBody);			
   $("body").append($simpleAlert);
-  $simpleAlertBody.show().animate({"marginTop":"-128px","opacity":"1"},300);		
-//		绑定事件
+  $simpleAlertBody.show().animate({"marginTop" : "-128px" ,"opacity" : "1"}, 300);		
+// 绑定事件
   $(".simpleAlertBtn1").click(function(){
     callback(true);
 	$(".simpleAlert").remove();	
@@ -40,19 +40,19 @@ var myConfirm=function(context,callback,config){
 		
 	}
 $(function(){
-//提交
-  $("#dblChoseAlert").click(function(){
+// 提交
+  $("#dblChoseAlert").click( function () {
     myConfirm("确定确定确定确定",
 	  function (isConfirm) {
 	    if (isConfirm) {console.log('已提交');} 
 	  });
   });
-//删除
+// 删除
   $("#dblChoseAlert2").click(function(){
     myConfirm("是否要删除", function (isConfirm) {
       if (isConfirm) {console.log('开始删除该数据')} 
       else{
-      	console.log('取消了删除')
+      	console.log('取消了删除');
       }
   }, {
 		cancelText: '不删除了',
--- DOM study/日历/date.js
+++ DOM study/日历/date.js
@@ -1,55 +1,56 @@
-//今天的
-var nowDat=new Date();//今天时间
-var nowYear=nowDat.getFullYear();//今天年份
-var nowMonth=nowDat.getMonth();//今天月份
-//设置的
-var dat=new Date();//当前时间
+// 今天的
+var nowDate = new Date(); // 今天时间
+var nowYear = nowDate.getFullYear(); // 今天年份
+var nowMonth = nowDate.getMonth(); // 今天月份
 
+// 设置的日历的日期
+var date = new Date(); 
 
-function Show(){
-  var year=dat.getFullYear();//设置的当前年份
-  var month=dat.getMonth();//设置的当前月份
-  var thisDayDate=dat.getDate();//设置的当前日期
-  var thisWeekday=dat.getDay();//设置的当前星期
-  var thisFristDay=new Date(year,month,1);//这个月的第一天
-  var thisFristWeekday =thisFristDay.getDay();//这个月第一天的星期
-  var thisLastDay=new Date(year,month+1,0);//这个月最后一天
-  var thisLastWeekday=thisLastDay.getDay();//这个月最后一天的星期
-  var dateNum =thisLastDay.getDate();//这个月有多少天
-  var preLastDay = new Date(year, month, 0);//上个月最后一天
-  var preLastDayNum = preLastDay.getDate();//上个月的天数
-  var showLastMonth=preLastDayNum-thisFristWeekday+1;//要显示上个月的日期
-  var showNextMonth=7-thisLastWeekday-1;//要显示下个月天数
+//渲染页面
+function Show () {
+  var year = date.getFullYear(); // 设置的当前年份
+  var month = date.getMonth(); // 设置的当前月份
+  var thisDayDate = date.getDate(); // 设置的当前日期
+  var thisWeekday = date.getDay(); // 设置的当前星期
+  var thisFristDay = new Date(year, month, 1); // 这个月的第一天
+  var thisFristWeekday = thisFristDay.getDay(); // 这个月第一天的星期
+  var thisLastDay = new Date(year, month + 1, 0); // 这个月最后一天
+  var thisLastWeekday = thisLastDay.getDay(); // 这个月最后一天的星期
+  var dateNum = thisLastDay.getDate(); // 这个月有多少天
+  var preLastDay = new Date(year, month, 0); // 上个月最后一天
+  var preLastDayNum = preLastDay.getDate(); // 上个月的天数
+  var showLastMonth = preLastDayNum - thisFristWeekday + 1; // 要显示上个月的日期
+  var showNextMonth = 7 - thisLastWeekday - 1; // 要显示下个月天数
   $("#year").text(year);
-  $("#month").text((month+1)+"月");
-  for(var i=0;i<thisFristWeekday;i++){
-    $("#date").append('<li class="not"><p>'+(showLastMonth++)+'</p></li>');
+  $("#month").text((month + 1) + "月"); // 显示月份
+  for(var i = 0; i < thisFristWeekday; i++){
+    $("#date").append('<li class="not"><p>' + (showLastMonth++) + '</p></li>');
   }
-  for(var i=1;i<=dateNum;i++){ 
-    if(i==thisDayDate&&month==nowMonth&&year==nowYear){
+  for(var i = 1; i <= dateNum; i++){ 
+    if( i === thisDayDate && month === nowMonth && year === nowYear){
       $("#date").append('<li class="active"><p>'+i+'</p></li>');
     }else{
       $("#date").append('<li class="hover"><p>'+i+'</p></li>');
     }
   }
-  for(var i=1;i<=showNextMonth;i++){
+  for(var i = 1; i <= showNextMonth; i++){
     $("#date").append('<li class="not"><p>'+i+'</p></li>');
   }
   
 
 }
-$(function(){
+$(function () {
   Show();
   $("#next").click(function () { 
     $("#date li").remove();
-    dat.setMonth(dat.getMonth()+1);
+    date.setMonth(date.getMonth()+1); // 下个月
     console.log("next");
     Show(); 
   });
 
   $("#prev").click(function () { 
     $("#date li").remove();
-    dat.setMonth(dat.getMonth()-1);
+    date.setMonth( date.getMonth() - 1); // 上个月
     console.log("prev");
     Show(); 
   });
